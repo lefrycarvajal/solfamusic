510309dce449e3effd9efa833c6e28a1fc9ad57d
lefrycarvajal <75230437+lefrycarvajal@users.noreply.github.com>
2021-08-16 06:35:23 -0400

    Update conexion.php

--- conexion.php
+++ conexion.php
@@ -1,5 +1,5 @@
 <?php
 
-$conex = mysqli_connect("localhost","root","","dbsolfamusic");
+$conex = mysqli_connect("u6354r3es4optspf.cbetxkdyhwsb.us-east-1.rds.amazonaws.com","sla2amszrzgdgdat","ix3m7c3fbc63291s","x7vsrn882flqpnpm");
 
 ?>
